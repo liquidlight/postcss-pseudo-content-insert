c108c0e9565cd6aa7300e5f40f7e9a82e221d2f7
Mike Street <mike@liquidlight.co.uk>
2015-09-09 15:14:44 +0100

    Create plugin
    Write first version of plugin. Searchs for content, if none found
    it then adds an empty content block to the end!

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-pseudo-content-insert', function (opts) {
     opts = opts || {};
 
-    // Work with options here
+    // Define the pseudo elements to look for
+    var customPseudoExp = /(.*::?)(after|before)$/;
 
     return function (css, result) {
 
-        // Transform CSS AST here
+        // Step over each rule
+        css.walkRules(function(rule) {
+
+            // Set flag to false
+            var hasContent = false;
+
+            // Check if the rule selector matches customPseudoExp
+            if(customPseudoExp.test(rule.selector)) {
+                // Loop over declarations of the rule
+                rule.walkDecls(function transformDecl(decl) {
+                    // If the property = content, assign flag to true
+                    if(decl.prop == 'content')
+                        hasContent = true;
+                });
+
+                // If flag is still false, append an empty content
+                if(!hasContent)
+                    rule.append({
+                        prop:'content',
+                        value: "''"
+                    });
+            }
+        });
 
     };
 });

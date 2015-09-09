857f54ef22895f4e99bc9a54e52ac327ca7b2c03
Mike Street <mike@liquidlight.co.uk>
2015-09-09 17:13:19 +0100

    Handle comma deliminated (multiple) selectors per rule
    When looping through, it will add any mulitple selectors without
    a content prop to an array. If it can't find any content for the
    selector - then it will throw a warning.
    
    The warnings need to be improved, with line numbers and whatnot!
    
    Resolves #2
    Opens #3

--- index.js
+++ index.js
@@ -4,31 +4,70 @@ module.exports = postcss.plugin('postcss-pseudo-content-insert',
 function (opts) {
     opts = opts || {};
 
+    // Removes an element from an array.
+    // String value: the value to search and remove.
+    // return: an array with the removed element; false otherwise.
+    Array.prototype.remove = function(value) {
+        var idx = this.indexOf(value);
+        if (idx != -1) {
+            return this.splice(idx, 1);
+        }
+        return false;
+    }
+
+    // Function to check if
+    Array.prototype.contentPresent = function(selector) {
+        var hasContent = false;
+        if(customPseudoExp.test(selector)) {
+            rule.walkDecls(function transformDecl(decl) {
+                // If the property = content, assign flag to true
+                if(decl.prop === 'content')
+                    hasContent = true;
+            });
+        }
+        return hasContent;
+    }
+
     // Define the pseudo elements to look for
     var customPseudoExp = /(.*::?)(after|before)$/;
+    // Define array for seelectors with missing content
+    var contentAwaiting = [];
 
-    return function (css) {
+
+    return function (css, result) {
 
         // Step over each rule
         css.walkRules(function (rule) {
-
-            // Set flag to false
-            var hasContent = false;
-
-            // Check if the rule selector matches customPseudoExp
-            if(customPseudoExp.test(rule.selector)) {
-                // Loop over declarations of the rule
-                rule.walkDecls(function transformDecl(decl) {
-                    // If the property = content, assign flag to true
-                    if(decl.prop === 'content')
-                        hasContent = true;
+            // If there is more than one selector, loop over each
+            // and if there is content missing, add to array
+            if(rule.selectors.length > 1) {
+                rule.selectors.forEach(function(selector) {
+                    if(!selector.contentPresent)
+                        contentAwaiting.push(selector)
                 });
-
-                // If flag is still false, append an empty content
-                if(!hasContent)
-                    rule.append({ prop: 'content', value: '\'\'' });
             }
         });
+
+        // Step again
+        css.walkRules(function (rule) {
+            // Loop again, this time only rules that have 1 selector
+            // only. If content is missing, add it.
+            // Then remove the selector from the contentAwaiting array
+            if(rule.selectors.length == 1) {
+                if(!rule.selector.contentPresent)
+                    rule.append({ prop: 'content', value: '\'\'' });
+
+                contentAwaiting.remove(rule.selector)
+
+            }
+        });
+
+        // If there is stuff left without a content block, let the user know!
+        if(contentAwaiting.length) {
+            var contentAwaitingSt = contentAwaiting.join(', ');
+             result.warn('The following selectors do not have any content associated with them: ' + contentAwaitingSt);
+        }
+
 
     };
 });

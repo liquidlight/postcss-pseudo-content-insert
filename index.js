0907597ad35145c986bc29af3702fcb370b8d97a
Mike Street <mike@liquidlight.co.uk>
2015-09-10 10:17:35 +0100

    Lint & test JS

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ function (opts) {
     // Removes an element from an array.
     // String value: the value to search and remove.
     // return: an array with the removed element; false otherwise.
-    Array.prototype.remove = function(value) {
-        var idx = this.indexOf(value);
-        if (idx != -1) {
-            return this.splice(idx, 1);
+    function removeFromArray(arr, selector) {
+        var idx = arr.indexOf(selector);
+        if (idx !== -1) {
+            return arr.splice(idx, 1);
         }
         return false;
     }
 
     // Checks to see if a value exists in an array
-    Array.prototype.hasValue = function(value) {
-        var idx = this.indexOf(value);
-        if (idx != -1) {
+    function arrayHasValue(arr, value) {
+        var idx = arr.indexOf(value);
+        if (idx !== -1) {
             return true;
         }
         return false;
@@ -37,7 +37,7 @@ function (opts) {
             // If there is more than one selector, loop over each
             // and if there is content missing, add to array
             if(rule.selectors.length > 1) {
-                rule.selectors.forEach(function(selector) {
+                rule.selectors.forEach(function (selector) {
                     var hasContent = false;
                     if(customPseudoExp.test(selector)) {
                         rule.walkDecls(function transformDecl(decl) {
@@ -46,13 +46,11 @@ function (opts) {
                                 hasContent = true;
                         });
                     }
-                    if(!hasContent) {
-                        if(!contentPresent.hasValue(selector))
+                    if(!arrayHasValue(contentPresent, selector))
+                        if(!hasContent)
                             contentAwaiting.push(selector);
-                    } else {
-                        if(!contentPresent.hasValue(selector))
+                        else
                             contentPresent.push(selector);
-                    }
                 });
             }
         });
@@ -62,7 +60,7 @@ function (opts) {
             // Loop again, this time only rules that have 1 selector
             // only. If content is missing, add it.
             // Then remove the selector from the contentAwaiting array
-            if(rule.selectors.length == 1) {
+            if(rule.selectors.length === 1) {
                 var hasContent = false;
                 if(customPseudoExp.test(rule.selector)) {
                     rule.walkDecls(function transformDecl(decl) {
@@ -75,7 +73,7 @@ function (opts) {
                     rule.append({ prop: 'content', value: '\'\'' });
 
                 // Remove the selector from contentAwaiting array
-                contentAwaiting.remove(rule.selector)
+                removeFromArray(contentAwaiting, rule.selector);
 
             }
         });
@@ -83,7 +81,10 @@ function (opts) {
         // If there is stuff left without a content block, let the user know!
         if(contentAwaiting.length) {
             var contentAwaitingSt = contentAwaiting.join(', ');
-             result.warn('The following selectors do not have any content associated with them: ' + contentAwaitingSt);
+            result.warn(
+                'The following selectors do not have any content: ' +
+                contentAwaitingSt
+            );
         }
 
 
